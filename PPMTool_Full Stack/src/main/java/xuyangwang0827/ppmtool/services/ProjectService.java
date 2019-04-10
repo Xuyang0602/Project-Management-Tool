f6c736b4a159765397866433ed6c913e73fea9e0
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-10 12:18:55 -0700

    Validation for Unique Project identifiers

--- PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/services/ProjectService.java
+++ PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/services/ProjectService.java
@@ -3,6 +3,7 @@ package xuyangwang0827.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import xuyangwang0827.ppmtool.domain.Project;
+import xuyangwang0827.ppmtool.exceptions.ProjectIdException;
 import xuyangwang0827.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -12,7 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        // Logic
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
     }
 }
