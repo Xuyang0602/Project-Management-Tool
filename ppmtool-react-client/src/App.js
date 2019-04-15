86388cda402d3c6527cbcb22f5e5bb6c78bff054
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-15 15:11:53 -0700

    add react-router

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import ProjectItem from './components/Project/ProjectItem';
-import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import ProjectItem from "./components/Project/ProjectItem";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-        <ProjectItem />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import ProjectItem from "./Project/ProjectItem";
 
 export default class Dashboard extends Component {
   render() {
@@ -9,11 +11,10 @@ export default class Dashboard extends Component {
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
               <br />
-              <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                Create a Project
-              </a>
+              <CreateProjectButton />
               <br />
               <hr />
+              <ProjectItem />
             </div>
           </div>
         </div>
