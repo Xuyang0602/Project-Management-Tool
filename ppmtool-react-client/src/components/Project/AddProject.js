74716f65044c59ea71b0a1b5ef45d62126f265d6
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-19 15:24:14 -0700

    Creat Redux Store

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
-export default class AddProject extends Component {
+class AddProject extends Component {
   constructor() {
     super();
 
@@ -25,7 +28,7 @@ export default class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date
     };
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -99,3 +102,12 @@ export default class AddProject extends Component {
     );
   }
 }
+
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
