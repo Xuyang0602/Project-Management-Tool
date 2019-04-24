827a41ee7aa940bb68caf8a15a8766b26f8e8b06
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-24 15:37:42 -0700

    Build Relationship with Peojct and Backlog

--- PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/repositories/BacklogRepository.java
+++ PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/repositories/BacklogRepository.java
@@ -6,4 +6,6 @@ import xuyangwang0827.ppmtool.domain.Backlog;
 
 @Repository
 public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String identifier);
 }
--- PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/services/ProjectService.java
+++ PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/services/ProjectService.java
@@ -2,8 +2,10 @@ package xuyangwang0827.ppmtool.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xuyangwang0827.ppmtool.domain.Backlog;
 import xuyangwang0827.ppmtool.domain.Project;
 import xuyangwang0827.ppmtool.exceptions.ProjectIdException;
+import xuyangwang0827.ppmtool.repositories.BacklogRepository;
 import xuyangwang0827.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -12,9 +14,22 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project) {
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if (project.getId() == null) {
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            } else {
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+
             return projectRepository.save(project);
         } catch (Exception e) {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
