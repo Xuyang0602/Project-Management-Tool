37939fb0af0a2427392461ad0a5b0e0d890154e4
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-24 15:06:18 -0700

    Backlog and ProjectTask Entities

---
+++ PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package xuyangwang0827.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import xuyangwang0827.ppmtool.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
