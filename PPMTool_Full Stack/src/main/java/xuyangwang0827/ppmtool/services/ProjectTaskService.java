8f1ea9c35376cc093c5d67f81a92fde378e5f082
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-25 16:55:05 -0700

    Replace h2 with mySQL and handle project not found exception

--- PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/services/ProjectTaskService.java
+++ PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/services/ProjectTaskService.java
@@ -3,8 +3,11 @@ package xuyangwang0827.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import xuyangwang0827.ppmtool.domain.Backlog;
+import xuyangwang0827.ppmtool.domain.Project;
 import xuyangwang0827.ppmtool.domain.ProjectTask;
+import xuyangwang0827.ppmtool.exceptions.ProjectNotFoundException;
 import xuyangwang0827.ppmtool.repositories.BacklogRepository;
+import xuyangwang0827.ppmtool.repositories.ProjectRepository;
 import xuyangwang0827.ppmtool.repositories.ProjectTaskRepository;
 
 @Service
@@ -16,40 +19,51 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        // Exceptions: Project not found
 
-        // ProjectTasks to be added to a specific project, project != null, backlog exists
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
+        try {
+            // ProjectTasks to be added to a specific project, project != null, backlog exists
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
 
-        // Set the backlog to projectTask
-        projectTask.setBacklog(backlog);
+            // Set the backlog to projectTask
+            projectTask.setBacklog(backlog);
 
-        // We want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
-        Integer backlogSequence = backlog.getPTSequence();
+            // We want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
+            Integer backlogSequence = backlog.getPTSequence();
 
-        // Update the backlog Sequence
-        backlogSequence++;
-        backlog.setPTSequence(backlogSequence);
+            // Update the backlog Sequence
+            backlogSequence++;
+            backlog.setPTSequence(backlogSequence);
 
-        // Add Sequence to Project Task
-        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            // Add Sequence to Project Task
+            projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        // Initial priority when priority is null
-        if (projectTask.getPriority() == null) {
-            projectTask.setPriority(3);
+            // Initial priority when priority is null
+            if (projectTask.getPriority() == null) {
+                projectTask.setPriority(3);
+            }
+
+            // Initial status when status is null
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project bot Found");
         }
 
-        // Initial status when status is null
-        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-            projectTask.setStatus("TO_DO");
-        }
 
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id.toUpperCase());
+        if (project == null) throw new ProjectNotFoundException("Project with id: '" + id + "' does not exist");
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
