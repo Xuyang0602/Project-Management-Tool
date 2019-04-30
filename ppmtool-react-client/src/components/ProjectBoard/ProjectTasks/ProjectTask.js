2c227d6dab6da788ea9cb7ff6a99b1698cb1465b
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-04-30 16:56:28 -0700

    Update Project Task

--- ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ProjectTask extends Component {
   render() {
@@ -30,9 +31,14 @@ class ProjectTask extends Component {
           <p className="card-text text-truncate ">
             {project_task_prop.acceptanceCriteria}
           </p>
-          <a href="#" className="btn btn-primary">
+          <Link
+            to={`/updateProjectTask/${project_task_prop.projectIdentifier}/${
+              project_task_prop.projectSequence
+            }`}
+            className="btn btn-primary"
+          >
             View / Update
-          </a>
+          </Link>
 
           <button className="btn btn-danger ml-4">Delete</button>
         </div>
