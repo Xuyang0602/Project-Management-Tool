2cecfcb00982c5e3e8c7c2ab35a3ef32b47fad33
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-05-06 12:44:22 -0700

    JWT Pre-work

--- PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/repositories/UserRepository.java
+++ PPMTool_Full Stack/src/main/java/xuyangwang0827/ppmtool/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import xuyangwang0827.ppmtool.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+    User getById(Long id);
 }
