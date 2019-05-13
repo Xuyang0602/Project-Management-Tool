8d0fdb21c7697ecf49583958bd193e7593b49053
xuyangwang0827 <xuyangwang0827@gmail.com>
2019-05-13 16:23:19 -0700

    logout operation

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -17,6 +17,7 @@ import Login from "./components/UserManagement/Login";
 import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/securityActions";
 
 const jwtToken = localStorage.jwtToken;
 if (jwtToken) {
@@ -29,6 +30,7 @@ if (jwtToken) {
 
   const currentTime = Date.now() / 1000;
   if (decode_jwtToken.exp > currentTime) {
+    store.dispatch(logout());
     window.location.href = "/";
   }
 }
--- ppmtool-react-client/src/components/UserManagement/Login.js
+++ ppmtool-react-client/src/components/UserManagement/Login.js
@@ -18,10 +18,11 @@ class Login extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) this.setState({ errors: nextProps.errors });
+
     if (nextProps.security.validToken) {
       this.props.history.push("/dashboard");
     }
-    if (nextProps.errors) this.setState({ errors: nextProps.errors });
   }
 
   onChange(e) {
@@ -34,11 +35,11 @@ class Login extends Component {
       username: this.state.username,
       password: this.state.password
     };
-    console.log(loginRequest);
     this.props.login(loginRequest);
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
@@ -49,22 +50,32 @@ class Login extends Component {
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.username
+                    })}
                     placeholder="Username"
                     name="username"
                     value={this.state.username}
                     onChange={this.onChange}
                   />
+                  {errors.username && (
+                    <div className="invalid-feedback">{errors.username}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
